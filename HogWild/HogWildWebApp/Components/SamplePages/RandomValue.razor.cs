0618e7ddef650fd57e209bcf15033c67d23f3861
Sam Wu <swu@nait.ca>
2024-01-15 18:48:58 -0700

    End of Jan 15 class

---
+++ HogWild/HogWildWebApp/Components/SamplePages/RandomValue.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace HogWildWebApp.Components.SamplePages
+{
+    public partial class RandomValue
+    {
+        #region Define data fields
+        private string? myName;
+        private int oddEvenValue;
+
+        #endregion
+
+        private void GenerateRandomValue()
+        {
+            oddEvenValue = Random.Shared.Next(0, 25);
+            if (oddEvenValue % 2 == 0)
+            {
+                myName = $"James is even {oddEvenValue}";
+            } 
+            else
+            {
+                myName = null;
+            }
+
+            //InvokeAsync(StateHasChanged);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            GenerateRandomValue();
+        }
+
+    }
+}
