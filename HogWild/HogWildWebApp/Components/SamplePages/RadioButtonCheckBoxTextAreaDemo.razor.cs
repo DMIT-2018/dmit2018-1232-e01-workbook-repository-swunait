f4e8a54a0678ae4dce0e50150e1fe5717852eab9
Sam Wu <swu@nait.ca>
2024-01-17 18:11:15 -0700

    Added components for TextBoxes and RadioButtons

---
+++ HogWild/HogWildWebApp/Components/SamplePages/RadioButtonCheckBoxTextAreaDemo.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HogWildWebApp.Components.SamplePages
+{
+    public partial class RadioButtonCheckBoxTextAreaDemo
+    {
+        #region Radio Buttons, Checkboxes & Text Area
+        //  selected meal
+        private string meal;
+        private string[] meals = new string[] { "breakfast", "lunch", "dinner", "snacks" };
+        //  used to hold the value of the acceptance
+        private bool acceptanceBox;
+        // used to hold the value for the message body
+        private string messageBody;
+        #endregion
+
+        //  used to display any feedback to the end user.
+        private string feedback = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            meal = meals[2]; // dinner
+        }
+
+        // Handle the selection of the loop meal
+        private void HandleMealSelection(ChangeEventArgs e)
+        {
+            meal = e.Value.ToString();
+        }
+
+        //  This method is called when a user submits radio, check box and area text.
+        private void RadioCheckAreaSubmit()
+        {
+            // Combine various values and store them in the 'feedback' variable as a formatted string.
+            feedback = $"Meal {meal}; Acceptance {acceptanceBox}; Message {messageBody}";
+
+            // Trigger a UI update to reflect the changes made to the 'feedback' variable.
+            //InvokeAsync(StateHasChanged);
+        }
+    }
+}
---
+++ HogWild/HogWildWebApp/Components/SamplePages/TextBoxesDemo.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace HogWildWebApp.Components.SamplePages
+{
+    public partial class TextBoxesDemo
+    {
+        #region Text Boxes
+        //  email address
+        private string emailText;
+        //  password
+        private string passwordText;
+        //  date 
+        private DateTime? dateText = DateTime.Today;
+        #endregion
+
+        //  used to display any feedback to the end user.
+        private string feedback;
+
+        //  This method is called when a user submits text input.
+        private void TextSubmit()
+        {
+            // Combine the values of emailText, passwordText, and dateText into a feedback message.
+            feedback = $"Email {emailText}; Password {passwordText}; Date {dateText}";
+
+            // Trigger a re-render of the component to reflect the updated feedback.
+            //InvokeAsync(StateHasChanged);
+        }
+
+    }
+}
