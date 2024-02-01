3617096738ae5eab20abe26e6053d76d99a897e7
Sam Wu <swu@nait.ca>
2024-01-31 18:45:25 -0700

    End of Jan 31

---
+++ HogWild/HogWildSystem/BLL/WorkingVersionsService.cs
@@ -0,0 +1,37 @@
+﻿using HogWildSystem.DAL;
+using HogWildSystem.Entities;
+using HogWildSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogWildSystem.BLL
+{
+    public class WorkingVersionsService
+    {
+        private readonly HogWildContext? _hogWildContext;
+
+        internal WorkingVersionsService(HogWildContext? hogWildContext)
+        {
+            _hogWildContext = hogWildContext;
+        }
+
+        public WorkingVersionsView? GetWorkingVersion()
+        {
+            return _hogWildContext.WorkingVersions
+                    .Select(wv => new WorkingVersionsView
+                    {
+                        VersionId = wv.VersionId,
+                        Major = wv.Major,
+                        Minor = wv.Minor,
+                        Build = wv.Build,
+                        Revision = wv.Revision,
+                        AsOfDate = wv.AsOfDate,
+                        Comments = wv.Comments
+                    }).FirstOrDefault();
+        }
+
+    }
+}
---
+++ HogWild/HogWildSystem/ViewModels/WorkngVersionsView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogWildSystem.ViewModels
+{
+    public class WorkingVersionsView
+    {
+        public int VersionId { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public int Revision { get; set; }   
+        public DateTime AsOfDate { get; set; }
+        public string Comments { get; set; }    
+    }
+}
