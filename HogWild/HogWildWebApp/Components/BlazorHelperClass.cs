e72158f6d6eec4c6dbf30bbe66eaad0a346d8264
Sam Wu <swu@nait.ca>
2024-02-09 18:47:41 -0700

    End of Feb 9

---
+++ HogWild/HogWildWebApp/Components/BlazorHelperClass.cs
@@ -0,0 +1,14 @@
+﻿namespace HogWildWebApp.Components
+{
+    public static class BlazorHelperClass
+    {
+        public static Exception GetInnerException(Exception ex)
+        {
+            while(ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+            return ex;
+        }
+    }
+}
---
+++ HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerList.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using HogWildSystem.BLL;    // for CustomerService
+
+namespace HogWildWebApp.Components.Pages.SamplePages
+{
+    public partial class CustomerList
+    {
+        #region fields
+
+        private string lastName;
+        private string phoneNumber;
+        private string feedbackMessage;
+        private string errorMessage;
+
+        private bool hasFeedback => !string.IsNullOrWhiteSpace(feedbackMessage);
+        private bool hasError => !string.IsNullOrWhiteSpace(errorMessage);
+        private List<string> errorDetails = new();
+
+        #endregion
+
+        #region properties
+        [Inject]
+        //protected CustomerS
+
+        #endregion
+    }
+}
