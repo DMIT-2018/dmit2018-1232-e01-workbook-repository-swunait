ac60aa3aab44e1d0fff89e91985261e3e065e395
Sam Wu <swu@nait.ca>
2024-02-14 18:37:42 -0700

    Added Paginator

--- HogWild/HogWildSystem/BLL/CustomerService.cs
+++ HogWild/HogWildSystem/BLL/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using HogWildSystem.DAL;
+﻿using DMIT2018.Paginator;
+using HogWildSystem.DAL;
 using HogWildSystem.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -68,6 +69,58 @@ namespace HogWildSystem.BLL
                 })
                 .OrderBy(x => x.LastName)
                 .ToList();
+        }
+   
+        public Task<PagedResult<CustomerSearchView>> GetCustomers(
+            string lastName,
+            string phone,
+            int page,
+            int pageSize,
+            string sortColumn,
+            string direction
+            )
+        {
+            // Business Rules
+            // These are processing rules that need to be satisfied
+            // for valid data
+
+            // Rule: Both last name and phone number cannot be empty
+            // Rule: RemoveFromViewFlag must be false
+            if (string.IsNullOrWhiteSpace(lastName) && string.IsNullOrWhiteSpace(phone))
+            {
+                throw new ArgumentNullException("Please provide either a last name and/or phone number");
+            }
+
+            // Need to update parameters so we are not searching on an empty value.
+            // Otherwise, an empty string will return all records
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                lastName = Guid.NewGuid().ToString();
+            }
+
+            if (string.IsNullOrWhiteSpace(phone))
+            {
+                phone = Guid.NewGuid().ToString();
+            }
+
+            return Task.FromResult(_hogWildContext.Customers
+                .Where(x => (x.LastName.Contains(lastName)
+                             || x.Phone.Contains(phone))
+                            && !x.RemoveFromViewFlag)
+                .Select(x => new CustomerSearchView
+                {
+                    CustomerID = x.CustomerID,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    City = x.City,
+                    Phone = x.Phone,
+                    Email = x.Email,
+                    StatusID = x.StatusID,
+                    TotalSales = x.Invoices.Sum(x => x.SubTotal + x.Tax)
+                })
+                .AsQueryable()
+                .OrderBy(sortColumn, direction)
+                .ToPagedResult(page, pageSize));
         }
     }
 }
---
+++ HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerListWithPaginator.razor.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.AspNetCore.Components;
+using HogWildSystem.BLL;
+using HogWildSystem.ViewModels;
+using DMIT2018.Paginator;
+using System.Security.Cryptography;    // for CustomerService
+
+namespace HogWildWebApp.Components.Pages.SamplePages
+{
+    public partial class CustomerListWithPaginator
+    {
+        #region Fields
+
+        // The last name
+        private string lastName;
+
+        // The phone number
+        private string phoneNumber;
+
+        // The feedback message
+        private string feedbackMessage;
+
+        // The error message
+        private string errorMessage;
+
+        // has feedback
+        private bool hasFeedback => !string.IsNullOrWhiteSpace(feedbackMessage);
+
+        // has error
+        private bool hasError => !string.IsNullOrWhiteSpace(errorMessage);
+
+        // error details
+        private List<string> errorDetails = new();
+        #endregion
+
+        #region Paginator
+        private const int PAGE_SIZE = 10;
+        protected string SortField { get; set; } = "Owner";
+        protected string Direction { get; set; } = "desc";
+        protected int CurrentPage { get; set; } = 1;
+        protected PagedResult<CustomerSearchView> PaginatorCustomerSearch { get; set; } = new();
+
+        private async void Sort(string column)
+        {
+            Direction = SortField == column ? Direction == "asc" ? "desc" : "asc" : "asc";
+            SortField = column;
+            await Search();
+        }
+        private string GetSortColumn(string x)
+        {
+            return x == SortField ? Direction == "desc" ? "desc" : "asc" : "";
+        }
+        private string SetSortIcon(string columnName)
+        {
+            if (SortField != columnName)
+            {
+                return "fa fa-sort";
+            }
+            if (Direction == "asc")
+            {
+                return "fa fa-sort-up";
+            }
+            else
+            {
+                return "fa fa-sort-down";
+            }
+        }
+
+        #endregion
+
+        #region Properties
+        // Injects the CustomerService dependency.
+        [Inject]
+        protected CustomerService CustomerService { get; set; }
+
+        // Injects the NavigationManager dependency.
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        // Gets or sets the customers search view.
+        //protected List<CustomerSearchView> Customers { get; set; } = new();
+        #endregion
+
+        #region Methods
+
+        //  search for an existing customer
+        private async Task Search()
+        {
+            try
+            {
+                //  reset the error detail list
+                errorDetails.Clear();
+
+                //  reset the error message to an empty string
+                errorMessage = string.Empty;
+
+                //  reset feedback message to an empty string
+                feedbackMessage = String.Empty;
+
+                //  clear the customer list before we do our search
+                //Customers.Clear();
+            
+
+                if (string.IsNullOrWhiteSpace(lastName) && string.IsNullOrWhiteSpace(phoneNumber))
+                {
+                    throw new ArgumentException("Please provide either a last name and/or phone number");
+                }
+
+                PaginatorCustomerSearch = await CustomerService.GetCustomers(
+                    lastName, phoneNumber,
+                    CurrentPage, PAGE_SIZE, SortField, Direction);
+                await InvokeAsync(StateHasChanged);
+
+                if (PaginatorCustomerSearch.Results.Length > 0)
+                {
+                    feedbackMessage = "Search for customer(s) was successful";
+                }
+                else
+                {
+                    feedbackMessage = "No customer were found for your search criteria";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (ArgumentException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (AggregateException ex)
+            {
+                //  have a collection of errors
+                //  each error should be place into a separate line
+                if (!string.IsNullOrWhiteSpace(errorMessage))
+                {
+                    errorMessage = $"{errorMessage}{Environment.NewLine}";
+                }
+                errorMessage = $"{errorMessage}Unable to search for customer";
+                foreach (var error in ex.InnerExceptions)
+                {
+                    errorDetails.Add(error.Message);
+                }
+            }
+        }
+
+        //  new customer
+        private void New()
+        {
+
+        }
+
+        //  edit selected customer
+        private void EditCustomer(int customerID)
+        {
+
+        }
+
+        //  new invoice for selected customer
+        private void NewInvoice(int customerID)
+        {
+
+        }
+
+        #endregion
+    }
+}
