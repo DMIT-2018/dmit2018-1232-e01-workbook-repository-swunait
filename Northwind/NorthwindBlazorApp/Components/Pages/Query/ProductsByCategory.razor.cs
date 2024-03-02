185fcd7e7614ce61b37ca18645340e0a31938610
Sam Wu <swu@nait.ca>
2024-03-01 18:47:51 -0700

    End of March 1 class.

---
+++ Northwind/NorthwindBlazorApp/Components/Pages/Query/ProductsByCategory.razor.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Components;
+using NorthwindSystem.BLL;
+using NorthwindSystem.ViewModels;
+
+namespace NorthwindBlazorApp.Components.Pages.Query
+{
+    public partial class ProductsByCategory
+    {
+        // The feedback message
+        private string feedbackMessage;
+
+        // The error message
+        private string errorMessage;
+
+        // has feedback
+        private bool hasFeedback => !string.IsNullOrWhiteSpace(feedbackMessage);
+
+        // has error
+        private bool hasError => !string.IsNullOrWhiteSpace(errorMessage);
+
+        // error details
+        private List<string> errorDetails = new();
+
+        [Inject]
+        protected ProductService ProductService { get; set; }
+
+        private List<ProductByCategoryViewModel> queryResultList = new();
+
+        private int categoryIdSearchValue;
+
+        private void onSearch()
+        {
+            try
+            {
+                //  reset the error detail list
+                errorDetails.Clear();
+
+                //  reset the error message to an empty string
+                errorMessage = string.Empty;
+
+                //  reset feedback message to an empty string
+                feedbackMessage = String.Empty;
+
+                queryResultList = ProductService.GetByCategoryId(categoryIdSearchValue); ;
+
+                if (queryResultList.Count == 0)
+                {
+                    feedbackMessage = $"There are no products with category id of {categoryIdSearchValue}";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (ArgumentException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (AggregateException ex)
+            {
+                //  have a collection of errors
+                //  each error should be place into a separate line
+                if (!string.IsNullOrWhiteSpace(errorMessage))
+                {
+                    errorMessage = $"{errorMessage}{Environment.NewLine}";
+                }
+                errorMessage = $"{errorMessage}Unable to search for customer";
+                foreach (var error in ex.InnerExceptions)
+                {
+                    errorDetails.Add(error.Message);
+                }
+            }
+
+        }
+
+    }
+}
--- Northwind/NorthwindSystem/BLL/ProductService.cs
+++ Northwind/NorthwindSystem/BLL/ProductService.cs
@@ -1,4 +1,6 @@
 ﻿using NorthwindSystem.DAL;
+using NorthwindSystem.Entities;
+using NorthwindSystem.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,26 @@ namespace NorthwindSystem.BLL
         internal ProductService(NorthwindContext northwindContext)
         {
             _northwindContext = northwindContext;
+        }
+
+        public List<ProductByCategoryViewModel> GetByCategoryId(int categoryId)
+        {
+            // rules: categoryId must be > 0
+            if (categoryId <= 0)
+            {
+                throw new ArgumentNullException("CategoryId value must be greater than 0.");
+            }
+
+            return _northwindContext.Products
+                    .Where(p => p.CategoryID == categoryId)
+                    .Select(p => new ProductByCategoryViewModel
+                    {
+                        ProductId = p.ProductID,
+                        ProductName = p.ProductName,
+                        Price = p.UnitPrice ?? 0
+                    })
+                    .OrderByDescending(x => x.Price)
+                    .ToList();
         }
 
     }
